0b1ee6e1cea1ed72d4abe53d8bba291dad06cd50
vlk2022 <volodymyrkostiuk2021@gmail.com>
2024-05-02 21:27:05 +0300

    commit

---
+++ scraper.js
@@ -0,0 +1,24 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    const URL = 'https://pptr.dev/';
+    await page.goto(URL);
+
+    const allArticles = await page.evaluate(() => {
+       const article = document.querySelectorAll('article');
+
+
+       return Array.from(article).slice(0, 5).map((article) => {
+           const div = article.querySelector('div');
+           const h1 = div.querySelector('h1').innerText;
+           const h2 = div.querySelector('h2').innerText;
+           return {h1, h2};
+       });
+    });
+    console.log(allArticles);
+
+    await browser.close();
+})();

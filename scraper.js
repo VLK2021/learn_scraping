f69d28b3107406db49399219756226dbb76b47a1
vlk2022 <volodymyrkostiuk2021@gmail.com>
2024-05-03 07:10:46 +0300

    start

--- scraper.js
+++ scraper.js
@@ -2,7 +2,6 @@ const puppeteer = require('puppeteer');
 const fs = require('fs');
 const path = require('path');
 
-
 const dirPath = path.join(process.cwd(), 'data');
 const filePath = path.join(dirPath, 'classesArray.txt');
 
